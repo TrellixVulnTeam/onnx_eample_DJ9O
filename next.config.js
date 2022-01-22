89b4eeed4f8ca1e317ff22fbffea8abfe5224be6
Marcus0086 <marcgupta456@gmail.com>
2022-01-22 16:35:38 +0530

    example onnx

---
+++ next.config.js
@@ -0,0 +1,35 @@
+
+/** @type {import('next').NextConfig} */
+
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, { }) => {
+
+    config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.fallback = { fs: false };
+
+    config.plugins.push(
+      new NodePolyfillPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
+            to: 'static/chunks/pages',
+          }, {
+            from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+            to: 'static/chunks/pages',
+          },
+          {
+            from: './model/model.ort',
+            to: 'static/chunks/pages',
+          },
+        ],
+      }),
+    );
+
+    return config;
+  }
+}
